b68836989529d54bbf84b95a7bee33bcc01c2ae5
Tadahiko Kawana <donkysoft@gmail.com>
2015-05-11 13:50:36 +0900

    edit README.md create demomark.js create sample

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # demomark
 Javascript to mark a specific environment.
+
+本番環境以外（テスト環境やステージング環境等）をブラウザで閲覧した際に、ぱっと見で本番環境との違いを付ける為のjsファイルです。
+
+## 使い方
+
+### demomark.js を読み込む
+  <script src="demomark.js"></script>
+読み込む位置はどこでもOK。
+
+### demomark.js を必要に応じて編集する
+- デフォルトでは、URL中の development. test. stage. staging. に反応します。
+- environments に配列追加したり、表示位置変更したり出来ます。
+
+## サンプル
+サンプルの各HTMLの内容は全て同一です。
+- [本番環境](sample/index.html）
+- [テスト環境](sample/test.html)
+- [ステージ環境](sample/stage.html)
---
+++ demomark.js
@@ -0,0 +1,40 @@
+(function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    // settings
+    var environments = [
+      {
+        url_include: 'test.',
+        text: 'test',
+        align: 'right', // left or right
+        valign: 'top' // top or bottom
+      },
+      {
+        url_include: 'stage.',
+        text: 'stage',
+        align: 'right', // left or right
+        valign: 'top', // top or bottom
+      },
+      {
+        url_include: 'staging.',
+        text: 'staging',
+        align: 'right', // left or right
+        valign: 'top', // top or bottom
+      },
+      {
+        url_include: 'development.',
+        text: 'development',
+        align: 'right', // left or right
+        valign: 'top', // top or bottom
+      }
+    ];
+    // exec
+    for (var i = 0; i < environments.length; i++) {
+      if (location.href.indexOf(environments[i].url_include) !== -1) {
+        var e = document.createElement('div');
+        e.innerHTML = '<div style="margin:0; padding:6px 12px; font-size:13px; font-family:sans-serif; display:inline-block; position:fixed; ' + environments[i].valign + ':0; ' + environments[i].align + ':0; background:#333; color:#FFF; font-weight:bold; text-align:center; line-height:1.1;">' + environments[i].text + '</div>';
+        document.body.appendChild(e);
+      }
+    }
+    
+  });
+})();
